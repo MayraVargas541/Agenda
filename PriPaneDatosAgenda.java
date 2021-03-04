4f1e1ef658879771b7bab0f344f93ca4e4c9f979
MayraVargas541 <78883269+MayraVargas541@users.noreply.github.com>
2021-03-03 18:58:25 -0600

    Delete PriPaneDatosAgenda.java

--- PriPaneDatosAgenda.java
+++
@@ -1,7 +0,0 @@
-public class PriPaneDatosAgenda{
-   public static void main(String args[]){
-      //JFrameDatos jfd = new JFrameDatos();
-      JFrameLogin JFL = new JFrameLogin();
-      
-   }
-}
